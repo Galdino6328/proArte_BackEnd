0a588d2457b92add8079ad6f066f7ae76a182ac5
Galdino6328 <34602227+Galdino6328@users.noreply.github.com>
2020-03-04 01:20:33 -0300

    update template ejs

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const path = require("path");
 
 //Routes
 const paymentsRoute = require("./src/routes/paymentsRoute");
-//const paymentsController = require("./src/controllers/paymentsController");
 
 //Load environment
 require("./src/config/getEnv")();
@@ -15,31 +14,15 @@ const app = express();
 
 app.use(cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
 });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// app.get(
-//   "/payments/checkout/:id/:email/:description/:amount",
-//   paymentsController.checkout
-// );
-// app.get("/success", (req, res) => {
-//   return res.render("success_screen");
-// });
-
-// app.get("/pending", (req, res) => {
-//   return res.render("pending_screen");
-// });
-
-// app.get("/failure", (req, res) => {
-//   return res.render("failure_screen");
-// });
 
 //views
 app.engine("ejs", engines.ejs);
@@ -50,6 +33,6 @@ app.set("view engine", "ejs");
 app.use("/payments", paymentsRoute);
 
 app.listen(process.env.API_PORT || 3001, function(err) {
-  if (err) console.error(err);
-  console.log(`API INICIADA NA PORTA ${process.env.API_PORT} 🚀`);
+    if (err) console.error(err);
+    console.log(`API INICIADA NA PORTA ${process.env.API_PORT} 🚀`);
 });
--- src/routes/paymentsRoute.js
+++ src/routes/paymentsRoute.js
@@ -4,20 +4,20 @@ const routes = express.Router();
 const paymentsController = require("../controllers/paymentsController");
 
 routes.get(
-  "/checkout/:id/:email/:description/:amount",
-  paymentsController.checkout
+    "/checkout/:id/:email/:description/:amount",
+    paymentsController.checkout
 );
 
 routes.get("/success", (req, res) => {
-  return res.render("success_screen");
+    return res.render("success_screen");
 });
 
 routes.get("/pending", (req, res) => {
-  return res.render("pending_screen");
+    return res.render("pending_screen");
 });
 
 routes.get("/failure", (req, res) => {
-  return res.render("failure_screen");
+    return res.render("failure_screen");
 });
 
 module.exports = routes;
