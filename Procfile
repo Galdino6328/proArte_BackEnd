9a9f2259f8d0600d63f098d7c3c5584ef084ce94
Galdino6328 <34602227+Galdino6328@users.noreply.github.com>
2020-03-22 18:55:25 -0300

    update env

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node src/index.js --e testing
+web: node src/index.js
--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+require("dotenv").config();
 const express = require("express");
+const port = process.env.PORT || 3332;
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const engines = require("consolidate");
 const path = require("path");
 
 //Routes
 const paymentsRoute = require("./src/routes/paymentsRoute");
 
-//Load environment
-require("./src/config/getEnv")();
+//db
+const db = mongoose.connection;
+const mongoURL = process.env.MONGO_URL || "mongodb://localhost/idPay";
+
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+mongoose.set("useCreateIndex", true);
+
+// //Load environment
+// require("./src/config/getEnv")();
 
 const app = express();
 
@@ -32,7 +46,14 @@ app.set("view engine", "ejs");
 //Payments route
 app.use("/payments", paymentsRoute);
 
-app.listen(process.env.API_PORT || 3001, function(err) {
-    if (err) console.error(err);
-    console.log(`API INICIADA NA PORTA ${process.env.API_PORT} 🚀`);
+app.listen(port, () => {
+    db.on("error", console.error.bind(console, "something got wrong! 💀"));
+    db.once("open", function() {
+        console.log("successful connection 🚀");
+        console.log(`server is  listening on port ${port}! 🚀 `);
+    });
 });
+// app.listen(process.env.API_PORT || 3001, function(err) {
+//     if (err) console.error(err);
+//     console.log(`API INICIADA NA PORTA ${process.env.API_PORT} 🚀`);
+// });
